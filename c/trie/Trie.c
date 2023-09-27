a84637f2ef8f93d82b950dec3fdfeb0f714cf4ad
SpinoniRaffaele <raffaelespinoni@gmail.com>
2023-09-27 15:49:13 +0200

    fix typo in trie definition

--- c/trie/Trie.c
+++ c/trie/Trie.c
@@ -4,7 +4,7 @@
 
 Trie *trie_init() {
     Trie *trie = (Trie *) malloc(sizeof(Trie));
-    trie->num_of_worlds = 0;
+    trie->num_of_words = 0;
     for (int i = 0; i < TRIE_NUM_OF_CHARACTER; i++) {
         trie->children[i] = NULL;
     }
@@ -51,7 +51,7 @@ void trie_insert(Trie *trie, char *elem) {
         cursor = next_node;
         index++;
     }
-    cursor->num_of_worlds++;
+    cursor->num_of_words++;
 }
 
 /**
@@ -64,8 +64,8 @@ int trie_simple_delete(Trie *trie, char *elem) {
         cursor = cursor->children[trie_mapping_char(elem[index])];
         index++;
     }
-    if (cursor->num_of_worlds > 0) {
-        cursor->num_of_worlds--;
+    if (cursor->num_of_words > 0) {
+        cursor->num_of_words--;
         return 0;
     }
     return 1;
@@ -80,16 +80,16 @@ int trie_delete(Trie *trie, char *elem) {
     int bigger_prefix_index = index;
     Trie *bigger_prefix_cursor = trie;
     while(elem[index] != '\0' && cursor != NULL) {
-        if (cursor->num_of_worlds > 0) {
+        if (cursor->num_of_words > 0) {
             bigger_prefix_cursor = cursor;
             bigger_prefix_index = index;
         }
         cursor = cursor->children[trie_mapping_char(elem[index])];
         index++;
     }
-    if (cursor->num_of_worlds > 0) {
-        cursor->num_of_worlds--;
-        if (is_leaf(cursor) && cursor->num_of_worlds == 0) {
+    if (cursor->num_of_words > 0) {
+        cursor->num_of_words--;
+        if (is_leaf(cursor) && cursor->num_of_words == 0) {
             trie_free_up(elem, bigger_prefix_cursor, bigger_prefix_index);
         }
         return 0;
@@ -111,14 +111,14 @@ int trie_search(Trie *trie, char *elem) {
         cursor = next_node;
         index++;
     }
-    if (cursor->num_of_worlds > 0) return 1;
+    if (cursor->num_of_words > 0) return 1;
     return 0;
 }
 
 void trie_print(Trie *trie) {
     for (int i = 0; i < TRIE_NUM_OF_CHARACTER; i++) {
         if (trie->children[i] != NULL) {
-            printf("Node: %c with %d worlds\n", trie_mapping_int(i), trie->children[i]->num_of_worlds);
+            printf("Node: %c with %d worlds\n", trie_mapping_int(i), trie->children[i]->num_of_words);
             trie_print(trie->children[i]);
         }
     }
--- c/trie/Trie.h
+++ c/trie/Trie.h
@@ -1,7 +1,7 @@
 #define TRIE_NUM_OF_CHARACTER 26    //lower letters of the english alphabet 
 
 typedef struct Trie_t {
-    int num_of_worlds;
+    int num_of_words;
     struct Trie_t *children[TRIE_NUM_OF_CHARACTER];
 } Trie;
 
