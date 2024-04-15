8b8a3ea6aa66b20eb1b8dc6ae95019c297ac4875
SpinoniRaffaele <raffaelespinoni@gmail.com>
2024-04-15 17:31:22 +0200

    add more graph algorithms

--- typescript/Graph/Graph.ts
+++ typescript/Graph/Graph.ts
@@ -1,13 +1,13 @@
 import { Queue } from '../Queue/Queue';
 
-export class Graph {
-  adjacencyList = new Map<Node, Node[]>();
+export class Graph<T> {
+  adjacencyList = new Map<Node<T>, Node<T>[]>();
 
-  constructor(adjacencyList: Map<Node, Node[]>) {
+  constructor(adjacencyList: Map<Node<T>, Node<T>[]>) {
     this.adjacencyList = adjacencyList;
   }
 
-  public dfs(root: Node, visited: Node[] = []) {
+  public dfs(root: Node<T>, visited: Node<T>[] = []) {
     console.log(root.value);
     visited.push(root);
 
@@ -16,8 +16,26 @@ export class Graph {
     });
   }
 
-  public bfs(root: Node, queued: Node[] = []) {
-    const queue = new Queue<Node>();
+  public dfsIterative(root: Node<T>) {
+    const stacked = [];
+    const stack = [];
+    stack.push(root);
+    stacked.push(root);
+
+    while(stack.length > 0) {
+      const elem = stack.pop();
+      console.log(elem.value);
+      this.adjacencyList.get(elem)?.forEach(neighbour => {
+        if(!stacked.includes(neighbour)) {
+          stack.push(neighbour);
+          stacked.push(neighbour);
+        }
+      });
+    }
+  }
+
+  public bfs(root: Node<T>, queued: Node<T>[] = []) {
+    const queue = new Queue<Node<T>>();
 
     queue.enqueue(root);
     queued.push(root);
@@ -33,12 +51,73 @@ export class Graph {
       });
     }
   }
+
+  //this suppose undirected graph
+  public bidirectionalSearch(node1: Node<T>, node2: Node<T>): T[] {
+    const queue1 = new Queue<Node<T>>();
+    const queued1: Node<T>[] = [];
+    const queue2 = new Queue<Node<T>>();
+    const queued2: Node<T>[] = [];
+    const path1 = [];
+    const path2 = [];
+
+    queue1.enqueue(node1);
+    queued1.push(node1);
+    queue2.enqueue(node2);
+    queued2.push(node2);
+    let intersectedElem = undefined;
+
+    while(!intersectedElem && !(queue1.isEmpty() && queue2.isEmpty())) {
+      let element1, element2;
+      if (!queue1.isEmpty()) {
+        element1 = queue1.dequeue();
+        path1.push(element1.value);
+      }
+
+      if (!queue2.isEmpty()) {
+        element2 = queue2.dequeue();
+        path2.push(element2.value);
+      }
+      intersectedElem = path1.filter(elem => path2.includes(elem))[0];
+
+      if (intersectedElem) {
+        const indexOfIntersection1 = path1.indexOf(intersectedElem);
+        const indexOfIntersection2 = path2.indexOf(intersectedElem);
+        const result = [];
+        for(let i = 0; i <= indexOfIntersection1; i++) {
+          result.push(path1[i]);
+        }
+        for(let i = indexOfIntersection2 - 1; i >= 0; i--) {
+          result.push(path2[i]);
+        }
+        return result;
+      } else {
+        if (element1) {
+          this.adjacencyList.get(element1)?.forEach(neighbour => {
+            if (!queued1.includes(neighbour)) {
+              queue1.enqueue(neighbour);
+              queued1.push(neighbour);
+            }
+          });
+        }
+        if (element2) {
+          this.adjacencyList.get(element2)?.forEach(neighbour => {
+            if (!queued2.includes(neighbour)) {
+              queue2.enqueue(neighbour);
+              queued2.push(neighbour);
+            }
+          });
+        }
+      }
+    }
+    return [];
+  }
 }
 
-export class Node {
-  public value: number;
+export class Node<T> {
+  public value: T;
 
-  constructor(value: number) {
+  constructor(value: T) {
     this.value = value;
   }
 }
--- typescript/Graph/TestGraph.ts
+++ typescript/Graph/TestGraph.ts
@@ -1,7 +1,7 @@
 import { Graph, Node } from './Graph';
 
 const testGraph = () => {
-  const adjacencyList = new Map<Node, Node[]>();
+  const adjacencyList = new Map<Node<number>, Node<number>[]>();
   const root = new Node(1);
   const e2 = new Node(2);
   const e3 = new Node(3);
@@ -10,15 +10,22 @@ const testGraph = () => {
   const e6 = new Node(6);
 
   adjacencyList.set(root, [e2, e3]);
-  adjacencyList.set(e3, [e4, e5]);
-  adjacencyList.set(e2, [e5, e6]);
+  adjacencyList.set(e3, [e4, e5, root]);
+  adjacencyList.set(e2, [e5, e6, root]);
+  adjacencyList.set(e5, [e6, e3]);
   adjacencyList.set(e4, [e6]);
+  adjacencyList.set(e6, [e2, e5]);
 
   const graph = new Graph(adjacencyList);
   console.log("DEPTH");
   graph.dfs(root);
+  console.log("DEPTH ITERATIVE");
+  graph.dfsIterative(root);
   console.log("BREADTH");
   graph.bfs(root);
+
+  console.log("BIDIRECTIONAL SEARCH");
+  console.log(graph.bidirectionalSearch(root, e6));
 };
 
 testGraph();
